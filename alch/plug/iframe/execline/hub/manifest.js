2082496565c04a5939cef975f54f8b70f4bfaea3
No32Ge <127807254+No32Ge@users.noreply.github.com>
2026-09-23 15:03:44 +0800

    x

--- alch/plug/iframe/execline/hub/manifest.js
+++ alch/plug/iframe/execline/hub/manifest.js
@@ -13,5 +13,5 @@ window.__EXECLINE_PRESET_INDEX__ = [
         desc: '组合成符合沃尔玛上架的结构（美国UPC）',
         // 实际的大体积 JSON 路径（占位符）
         url: 'https://www.ge32.cc/alch/plug/iframe/execline/hub/rules/Orbix_USA_UPC.json'
-    }json
+    }
 ];
